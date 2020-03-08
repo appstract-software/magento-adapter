cfeb828d72e0308b7533abec34a41d59f3f9f973
szuti <piotr.szuttenbach@appstract.software>
2020-03-08 22:40:20 +0100

    ProductsRepositoryInterface doc

--- Api/ProductsRepositoryInterface.php
+++ Api/ProductsRepositoryInterface.php
@@ -4,5 +4,11 @@ namespace Appstractsoftware\MagentoAdapter\Api;
 
 interface ProductsRepositoryInterface
 {
+  /**
+   * Get most viewed products
+   *
+   * @param int $limit
+   * @return \Magento\Catalog\Api\Data\ProductSearchResultsInterface
+   */
   public function getList($limit = 10);
 }
