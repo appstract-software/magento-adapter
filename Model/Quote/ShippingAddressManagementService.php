cad79341ebd983c2f50851e0c14dbf9168c6c34e
Mateusz Lesiak <mateusz.lesiak@appstract.software>
2020-12-07 15:57:44 +0100

    Fix interface

--- Model/Quote/ShippingAddressManagementService.php
+++ Model/Quote/ShippingAddressManagementService.php
@@ -2,7 +2,7 @@
 
 namespace Appstractsoftware\MagentoAdapter\Model\Quote;
 
-use Appstractsoftware\MagentoAdapter\Api\ShippingAddressManagementServiceInterface;
+use Appstractsoftware\MagentoAdapter\Api\Quote\ShippingAddressManagementServiceInterface;
 use \Magento\Quote\Model\ShippingAddressManagement as ShippingAddressManagement;
 
 class ShippingAddressManagementService extends ShippingAddressManagement implements ShippingAddressManagementServiceInterface
