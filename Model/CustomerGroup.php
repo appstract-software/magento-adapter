109fb4370781f1b7286f10b27313a2455a1ed985
Bartek Szafarek <bartosz.szafarek@appstract.software>
2021-01-30 23:52:02 +0100

    d

--- Model/CustomerGroup.php
+++ Model/CustomerGroup.php
@@ -7,16 +7,24 @@ use Magento\Quote\Model\GuestCart\GuestCartRepository;
 
 class CustomerGroup extends GuestCartRepository implements CustomerGroupInterface
 {
-  /**
-   * @inheritDoc
-   */
+  // /**
+  //  * @inheritDoc
+  //  */
 
+   
   public function setCustomerGroup($cartId, $customerGroupId)
   {
-    $cart = $this->get($cartId);
-    $cart->setData('customer_group_id', $customerGroupId);
-    $this->quoteRepository->save($cart);
+    try {
+        $cart = $this->get($cartId);
+        $cart->setData('customer_group_id', $customerGroupId);
+        $this->quoteRepository->save($cart);
+        $response = ['success' => 'true'];
+    } catch (\Exception $e) {
+        $response = ['error' => 'true', 'message' => $e->getMessage()];
+    }
 
-    return 'success';
+    // TODO: Change this to JsonFactory from Magento
+    return json_encode($response);
+
   }
 }
