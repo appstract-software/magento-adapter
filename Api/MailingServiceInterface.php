fcaa6137cc803266d35b73b5b30b644949188983
Bartek Szafarek <bartosz.szafarek@appstract.software>
2021-04-07 10:20:30 +0200

    Mailing Service

---
+++ Api/MailingServiceInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Appstractsoftware\MagentoAdapter\Api;
+
+/**
+ * Newsletter interface.
+ * @api
+ */
+interface MailingServiceInterface
+{
+    /**
+     * Subscribe an email.
+     *
+     * @param string $mail
+     * @param string $name
+     * @param string $templateId
+     * @return \Appstractsoftware\MagentoAdapter\Api\MailingServiceInterface
+     */
+    public function sendMail($mail, $name, $templateId);
+}
+
+
+
+
+
