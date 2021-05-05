7b95997425b351e1b44ede896cf5257ca8cc0550
Bartosz Wróbel <bartoszwrobel14@gmail.com>
2021-05-05 12:25:30 +0200

    product item repository support for configurable type (#34)

--- Plugin/ProductItemRepository.php
+++ Plugin/ProductItemRepository.php
@@ -8,31 +8,43 @@ use Appstractsoftware\MagentoAdapter\Api\Data\ProductUnitsInterface;
 use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use \Magento\Framework\Api\SearchResults;
+use \Magento\InventorySalesApi\Api\Data\SalesChannelInterface;
 
 class ProductItemRepository
 {
     /** @var \Appstractsoftware\MagentoAdapter\Api\Data\ProductPriceInterface */
     private $productPriceLoader;
 
-    /** @var \Appstractsoftware\MagentoAdapter\Api\Data\ProductUnitsInterface */
-    private $productUnits;
-
     /** @var \Magento\CatalogInventory\Api\StockRegistryInterface */
     private $stockRegistry;
 
+    private $sourcesCache = array();
+
     /**
      * ProductRepository constructor.
-    *
-    * @param ProductPriceInterface $productPriceLoader
-    */
+     *
+     * @param ProductPriceInterface $productPriceLoader
+     */
     public function __construct(
         ProductPriceInterface $productPriceLoader,
         ProductUnitsInterface $productUnitsLoader,
-        \Magento\CatalogInventory\Api\StockRegistryInterface $stockRegistry
+        \Magento\CatalogInventory\Model\Spi\StockRegistryProviderInterface $stockRegistry,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\InventorySalesApi\Api\GetProductSalableQtyInterface $getProductSalableQty,
+        \Magento\InventorySalesApi\Api\IsProductSalableInterface $isProductSalable,
+        \Magento\InventorySalesApi\Api\StockResolverInterface $stockResolver,
+        \Magento\InventoryApi\Api\GetSourceItemsBySkuInterface $sourceItemsBySku,
+        \Magento\InventoryApi\Api\SourceRepositoryInterface $sourceRepository
     ) {
         $this->productPriceLoader = $productPriceLoader;
         $this->productUnitsLoader = $productUnitsLoader;
         $this->stockRegistry = $stockRegistry;
+        $this->storeManager = $storeManager;
+        $this->getProductSalableQty = $getProductSalableQty;
+        $this->stockResolver = $stockResolver;
+        $this->isProductSalable = $isProductSalable;
+        $this->sourceItemsBySku = $sourceItemsBySku;
+        $this->sourceRepository = $sourceRepository;
     }
 
     /**
@@ -44,7 +56,9 @@ class ProductItemRepository
      */
     public function afterGetById(ProductRepositoryInterface $subject, ProductInterface $product)
     {
-        return $this->loadData($product);
+        $stockId = $this->getStockId();
+
+        return $this->loadData($subject, $product, $stockId);
     }
 
     /**
@@ -56,7 +70,9 @@ class ProductItemRepository
      */
     public function afterGet(ProductRepositoryInterface $subject, ProductInterface $product)
     {
-        return $this->loadData($product);
+        $stockId = $this->getStockId();
+
+        return $this->loadData($subject, $product, $stockId);
     }
 
     /**
@@ -66,18 +82,20 @@ class ProductItemRepository
      * @param SearchResults $searchCriteria
      * @return SearchResults
      */
-    public function afterGetList(ProductRepositoryInterface $subject, SearchResults $searchCriteria) : SearchResults
+    public function afterGetList(ProductRepositoryInterface $subject, SearchResults $searchCriteria): SearchResults
     {
+        $stockId = $this->getStockId();
+
         $products = [];
         foreach ($searchCriteria->getItems() as $product) {
-            $products[] = $this->loadData($product);
+            $products[] = $this->loadData($subject, $product, $stockId);
         }
-        if (!empty($products[0]) && $searchCriteria->getTotalCount() === 1) {
-            $stockItem = $this->stockRegistry->getStockItem($products[0]->getId());
-            $extensionAttributes = $products[0]->getExtensionAttributes();
-            $extensionAttributes->setStockItem($stockItem);
-            $products[0]->setExtensionAttributes($extensionAttributes);
-        }
+        // if (!empty($products[0]) && $searchCriteria->getTotalCount() === 1) {
+        //     $stockItem = $this->stockRegistry->getStockItem($products[0]->getId());
+        //     $extensionAttributes = $products[0]->getExtensionAttributes();
+        //     $extensionAttributes->setStockItem($stockItem);
+        //     $products[0]->setExtensionAttributes($extensionAttributes);
+        // }
         $searchCriteria->setItems($products);
         return $searchCriteria;
     }
@@ -85,11 +103,15 @@ class ProductItemRepository
     /**
      * Load extension attribute data.
      *
+     * @param ProductRepositoryInterface $subject
      * @param ProductInterface $product
+     * @param int $stockId
      * @return ProductInterface
      */
-    private function loadData($product) {
-        $typeInstance = $product->getTypeInstance(true);
+    private function loadData($subject, $product, $stockId)
+    {
+        $typeInstance = $product->getTypeInstance();
+
         if (!empty($typeInstance)) {
             $extensionAttributes = $product->getExtensionAttributes();
 
@@ -99,9 +121,127 @@ class ProductItemRepository
             $units = clone $this->productUnitsLoader->load($product);
             $extensionAttributes->setUnits($units);
 
+            $sources = array();
+            $stockItem = $extensionAttributes->getStockItem();
+
+            if ($stockItem == null) {
+                $stockItem = $this->stockRegistry->getStockItem($product->getId(), null);
+            }
+
+            $stockItem->setStockId($stockId);
+
+            $productType = $product->getTypeId();
+
+            if ($productType == 'configurable') {
+                $stockItems = array();
+                $sourceItems = array();
+
+                $configurableProductStockStatus = false;
+                $configurableProductStockQty = 0;
+
+                $configurableProductsLinks = $extensionAttributes->getConfigurableProductLinks();
+
+                foreach ($configurableProductsLinks as $link) {
+                    $configurableProduct = $subject->getById($link);
+                    $configurableProductStockItem = $configurableProduct->getExtensionAttributes()->getStockItem();
+                    $configurableProductSourceItems = $this->getSourceItems($configurableProduct->getSku());
+                    $sourceItems = array_merge($sourceItems, $configurableProductSourceItems);
+
+                    foreach ($sourceItems as $sourceItem) {
+                        $sources[] = $this->getSource($sourceItem->getSourceCode());
+                    }
+
+                    $stockData = $this->getStockData($configurableProduct->getSku(), $stockId);
+
+                    $configurableProductStockItem->setIsInStock($stockData['status']);
+                    $configurableProductStockItem->setQty($stockData['qty']);
+
+                    $stockItems[] = $configurableProductStockItem;
+
+                    $configurableProductStockQty += $stockData['qty'];
+
+                    if ($stockData['status'] == true) {
+                        $configurableProductStockStatus = true;
+                    }
+                }
+
+                $stockItem->setIsInStock($configurableProductStockStatus);
+                $stockItem->setQty($configurableProductStockQty);
+
+                $extensionAttributes->setStockItems($stockItems);
+                $extensionAttributes->setSourceItems($sourceItems);
+            } else if ($productType == 'simple') {
+                $stockData = $this->getStockData($product->getSku(), $stockId);
+                $stockItem->setIsInStock($stockData['status']);
+                $stockItem->setQty($stockData['qty']);
+
+                $sourceItems = $this->getSourceItems($product->getSku());
+                $extensionAttributes->setSourceItems($sourceItems);
+
+                foreach ($sourceItems as $sourceItem) {
+                    $sources[] = $this->getSource($sourceItem->getSourceCode());
+                }
+
+                $extensionAttributes->setSources($sources);
+            }
+
+            $extensionAttributes->setStockItem($stockItem);
+            $extensionAttributes->setSources(array_unique($sources, SORT_REGULAR));
+
             $product->setExtensionAttributes($extensionAttributes);
         }
 
         return $product;
+    }
+
+    private function getStockData($sku, $stockId)
+    {
+        $qty = $this->getProductSalableQty->execute($sku, $stockId);
+        $status = $this->isProductSalable->execute($sku, $stockId);
+
+        return array('qty' => $qty, 'status' => $status);
+    }
+
+    /**
+     * Get stock id
+     *
+     * @return int
+     */
+    private function getStockId()
+    {
+        $websiteId = $this->storeManager->getStore()->getWebsiteId();
+        $websiteCode = $this->storeManager->getWebsite($websiteId)->getCode();
+
+        return $this->stockResolver->execute(SalesChannelInterface::TYPE_WEBSITE, $websiteCode)->getStockId();
+    }
+
+    /**
+     * Get source items by given sku
+     * 
+     * @param string $sku
+     * @return \Magento\InventoryApi\Api\Data\SourceItemInterface[]
+     */
+    private function getSourceItems($sku)
+    {
+        return $this->sourceItemsBySku->execute($sku);
+    }
+
+    /**
+     * Get source by given source code
+     * 
+     * @param string $sourceCode
+     * @return \Magento\InventoryApi\Api\Data\SourceInterface
+     */
+    private function getSource($sourceCode)
+    {
+
+        if (!array_key_exists($sourceCode, $this->sourcesCache)) {
+            $source = $this->sourceRepository->get($sourceCode);
+            $this->sourcesCache[$sourceCode] = $source;
+
+            return $source;
+        }
+
+        return $this->sourcesCache[$sourceCode];
     }
 }
