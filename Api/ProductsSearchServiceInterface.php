685bf1b3d40aa827f3823cde3160e315012b0f85
Bartosz Wróbel <bartoszwrobel14@gmail.com>
2021-05-27 14:35:25 +0200

    search by query endpoint

--- Api/ProductsSearchServiceInterface.php
+++ Api/ProductsSearchServiceInterface.php
@@ -5,10 +5,18 @@ namespace Appstractsoftware\MagentoAdapter\Api;
 interface ProductsSearchServiceInterface
 {
     /**
-     * Get products list
+     * Search products
      *
      * @param \Magento\Framework\Api\SearchCriteriaInterface $searchCriteria
      * @return \Appstractsoftware\MagentoAdapter\Api\Data\ProductsSearchResultsInterface
      */
     public function searchProducts(\Magento\Framework\Api\SearchCriteriaInterface $searchCriteria);
+
+    /**
+     * Search products by query
+     *
+     * @param \Magento\Framework\Api\Search\SearchCriteriaInterface $searchCriteria
+     * @return \Appstractsoftware\MagentoAdapter\Api\Data\ProductsSearchResultsInterface
+     */
+    public function searchProductsByQuery(\Magento\Framework\Api\Search\SearchCriteriaInterface $searchCriteria);
 }
