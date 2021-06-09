4d182f133fec8761916a9c2d11b91f6d46396a16
Bartosz Wróbel <bartoszwrobel14@gmail.com>
2021-06-09 15:36:59 +0200

    sources to skip

---
+++ Helper/Data.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Appstractsoftware\MagentoAdapter\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Store\Model\ScopeInterface;
+
+class Data extends AbstractHelper
+{
+
+  const XML_PATH_SECTION = 'appstract';
+  const XML_PATH_GROUP = 'appstract_configuration';
+  const AUTO_GENERATE_INVOICE = 'auto_generate_invoice';
+  const SOURCES_TO_SKIP = 'sources_to_skip';
+
+
+  public function getConfigValue($field, $storeId = null)
+  {
+    return $this->scopeConfig->getValue(
+      $field,
+      ScopeInterface::SCOPE_STORE,
+      $storeId
+    );
+  }
+
+  public function getConfiguration($field, $storeId = null)
+  {
+
+    return $this->getConfigValue(self::XML_PATH_SECTION . '/' . self::XML_PATH_GROUP . '/' . $field, $storeId);
+  }
+
+  public function getSourcesToSkip($storeId = null)
+  {
+    $sources = $this->getConfiguration(self::SOURCES_TO_SKIP, $storeId);
+    
+    if ($sources == '') {
+      return [];
+    }
+
+    return explode(',', $sources);
+  }
+}
