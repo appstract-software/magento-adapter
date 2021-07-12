817490c95d2d89aec50f0888a55d9e038b328e24
Kornel Krześlak <kornel.krzeslak@appstract@software>
2021-07-12 12:53:19 +0200

    Free delivery in totals ext attrs

---
+++ Plugin/TotalsInterface.php
@@ -0,0 +1,46 @@
+<?php
+namespace Appstractsoftware\MagentoAdapter\Plugin;
+
+use \Magento\Framework\App\Config\ScopeConfigInterface;
+use \Magento\Sales\Api\Data\TotalExtensionFactory;
+
+class TotalsInterface
+{
+    /**
+     * @var ScopeConfigInterface 
+     */
+    private $scopeConfig;
+
+    /**
+     * @var TotalExtensionFactory
+     */
+    private $totalExtensionFactory;
+
+    /**
+     * Initialize dependencies.
+     * 
+     * @param ScopeConfigInterface $scopeConfig
+     * @param TotalExtensionFactory $totalExtensionFactory
+     */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig,
+        TotalExtensionFactory $totalExtensionFactory
+    ) {
+        $this->scopeConfig = $scopeConfig;
+        $this->totalExtensionFactory = $totalExtensionFactory;
+    }
+
+    public function afterGetExtensionAttributes(\Magento\Quote\Model\Cart\Totals\Interceptor $totals, $result) {
+        $freeshipping =  $this->scopeConfig->getValue('carriers/freeshipping/free_shipping_subtotal', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        
+        if (!$result) {
+            $extensionAttributes = $this->totalExtensionFactory->create();
+            $extensionAttributes->setFreeShipping($freeshipping);
+            $totals->setExtensionAttributes($extensionAttributes);
+        } else {
+            $result->setFreeShipping($freeshipping);
+        }
+
+        return $result;
+    }
+}
