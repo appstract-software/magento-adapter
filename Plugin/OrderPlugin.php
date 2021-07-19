3951ffa91ca4b41bc0986c204c7ed0e1807ec6c0
BartoszSzafarek <61381599+BartoszSzafarek@users.noreply.github.com>
2021-07-19 14:24:29 +0200

    Feature/order aftersave (#59)
    Co-authored-by: Bartek Szafarek <bartosz.szafarek@appstract.software>
    Co-authored-by: Michał Okrasa <44529426+BalboBigguns@users.noreply.github.com>
    Co-authored-by: Mike <michal.okrasa98@gmail.com>

---
+++ Plugin/OrderPlugin.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Appstractsoftware\MagentoAdapter\Plugin;
+
+use \Magento\Sales\Api\OrderRepositoryInterface;
+
+class OrderPlugin
+{
+
+  /**
+   * [afterSave description]
+   * @param  \Magento\Sales\Model\ResourceModel\Order $subject [description]
+   * @param  [type]                                   $result  [description]
+   * @param  [type]                                   $object  [description]
+   * @return [type]                                            [description]
+   */
+  public function afterSave(
+    \Magento\Sales\Model\ResourceModel\Order $subject,
+    $result,
+    $object
+  ) {
+    return $object->getIncrementId();
+  }
+}
