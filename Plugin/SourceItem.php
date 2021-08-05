b9fcd0c9e19b5ce3847d3bf504d327bafe2ac529
BartoszSzafarek <61381599+BartoszSzafarek@users.noreply.github.com>
2021-08-05 14:08:13 +0200

    source items (#71)
    Co-authored-by: Bartek Szafarek <bartosz.szafarek@appstract.software>

---
+++ Plugin/SourceItem.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Appstractsoftware\MagentoAdapter\Plugin;
+
+use \Magento\Framework\Api\SearchResults;
+use Magento\Inventory\Model\SourceItem\Command\SourceItemsSave;
+
+class SourceItem
+{
+  private $eventManager;
+
+  public function __construct(\Magento\Framework\Event\ManagerInterface $eventManager)
+  {
+    $this->eventManager = $eventManager;
+  }
+
+  /**
+   * Add cartItem options to extension attributes.
+   * 
+   * @param \Magento\Inventory\Model\SourceItem\Command\SourceItemsSave $subject
+   * @param SearchResults $searchCriteria
+   * @return mixed
+   */
+  public function beforeExecute($subject, $sourceItems)
+  {
+    $sourceCodes = [];
+    $items = [];
+
+    foreach ($sourceItems as $item) {
+      if (!in_array($item->getSourceCode(), $sourceCodes)) {
+        $sourceCodes[] = $item->getSourceCode();
+        $items[] = $item;
+      }
+    }
+
+    foreach ($items as $item) {
+      $this->eventManager->dispatch('update_stock', ['sourceItem' => $item]);
+    }
+  }
+}
