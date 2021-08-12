8fef2ac2ecb0b3aa424bd7960972f8e356257927
Bartek Szafarek <bartosz.szafarek@appstract.software>
2021-08-12 12:19:08 +0200

    Order Details

--- Model/OrdersManagment.php
+++ Model/OrdersManagment.php
@@ -3,9 +3,19 @@
 namespace Appstractsoftware\MagentoAdapter\Model;
 
 use \Appstractsoftware\MagentoAdapter\Api\OrdersManagmentInterface;
+use Magento\Sales\Api\OrderRepositoryInterface;
 
 class OrdersManagment implements OrdersManagmentInterface
 {
+  /**
+   * @var \Magento\Store\Model\StoreManagerInterface
+   */
+  private $storeManager;
+
+  /**
+   * @var \Magento\Catalog\Api\ProductRepositoryInterface $apiProductRepository,
+   */
+  public $_productRepository;
 
   /**
    * @var \Magento\Sales\Api\OrderRepositoryInterface
@@ -22,14 +32,39 @@ class OrdersManagment implements OrdersManagmentInterface
    */
   public $_filterBuilder;
 
+  /**
+   * @var \Magento\Sales\Api\Data\OrderItemExtensionFactory
+   */
+  public $orderItemExtensionFactory;
+
+  /**
+   * @var \Magento\ConfigurableProduct\Model\Product\Type\Configurable
+   */
+  public $configurableProduct;
+
+  /**
+   * @var \Appstractsoftware\MagentoAdapter\Api\Data\OrderItemOptionsInterface
+   */
+  public $orderItemOptions;
+
   public function __construct(
+    \Magento\Sales\Api\Data\OrderItemExtensionFactory $orderItemExtensionFactory,
+    \Magento\ConfigurableProduct\Model\Product\Type\Configurable $configurableProduct,
+    \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
     \Magento\Sales\Api\OrderRepositoryInterface $orderRepository,
     \Magento\Framework\Api\SearchCriteriaBuilder $searchCriteriaBuilder,
-    \Magento\Framework\Api\FilterBuilder $filterBuilder
+    \Magento\Store\Model\StoreManagerInterface $storeManager,
+    \Magento\Framework\Api\FilterBuilder $filterBuilder,
+    \Appstractsoftware\MagentoAdapter\Api\Data\OrderItemOptionsInterface $orderItemOptions
   ) {
+    $this->orderItemExtensionFactory = $orderItemExtensionFactory;
+    $this->configurableProduct = $configurableProduct;
+    $this->_productRepository = $productRepository;
     $this->_orderRepository = $orderRepository;
     $this->_searchCriteriaBuilder = $searchCriteriaBuilder;
+    $this->storeManager = $storeManager;
     $this->_filterBuilder = $filterBuilder;
+    $this->orderItemOptions = $orderItemOptions;
   }
 
   /**
@@ -41,9 +76,167 @@ class OrdersManagment implements OrdersManagmentInterface
     $filters = [
       $this->_filterBuilder->setField('customer_id')->setValue($customerId)->create()
     ];
+    $searchCriteria = $this->_searchCriteriaBuilder->addFilters($filters)->create();
+    $items = $this->_orderRepository->getList($searchCriteria)->getItems();
+
+    $products = $this->getProducts($items);
+    $parents = $this->getParents($products);
+
+    foreach ($items as $order) {
+      foreach ($order->getItems() as $orderItem) {
+        foreach ($products as $product) {
+          if ($orderItem->getSku() === $product->getSku()) {
+            $this->assignExtensionAttributes($orderItem, $product, $parents);
+          }
+        }
+      }
+    }
+
+    return $items;
+  }
+
+  public function getProducts($items)
+  {
+    $skus = [];
+
+    foreach ($items as $item) {
+      foreach ($item->getItems() as $product) {
+        if (!in_array($product->getSku(), $skus)) {
+          $skus[] = $product->getSku();
+        }
+      }
+    }
+    $filters = [];
+
+    foreach ($skus as $sku) {
+      $filters[] = $this->_filterBuilder->setField('sku')->setValue($sku)->create();
+    }
+
+
+    $searchCriteria = $this->_searchCriteriaBuilder->addFilters($filters)->create();
+    $products = $this->_productRepository->getList($searchCriteria)->getItems();
+
+    return $products;
+  }
+
+  public function getParents($products)
+  {
+    $parentIds = [];
+
+    foreach ($products as $product) {
+      $parentIds = $this->configurableProduct->getParentIdsByChild($product->getId());
+
+      if (!empty($parentIds)) {
+        $parentId = array_shift($parentIds);
+        if (!in_array($parentId, $parentIds)) {
+          $parentIds[] = $parentId;
+        }
+      }
+    }
+
+
+    if (empty($parentIds)) {
+      return [];
+    }
+
+    $filters = [];
+
+    foreach ($parentIds as $id) {
+      $filters[] = $this->_filterBuilder->setField('entity_id')->setValue($id)->create();
+    }
 
     $searchCriteria = $this->_searchCriteriaBuilder->addFilters($filters)->create();
 
-    return $this->_orderRepository->getList($searchCriteria)->getItems();
+    $parents = $this->_productRepository->getList($searchCriteria)->getItems();
+
+    return $parents;
+  }
+
+
+  public function assignExtensionAttributes(
+    $orderItem,
+    $product,
+    $parents
+  ) {
+    $imageUrl = $this->getProductImage($product);
+
+    $orderItemExtension = $this->orderItemExtensionFactory->create();
+    $orderItemExtension->setProductImage($imageUrl);
+
+    if (!empty($parents)) {
+      $options = $this->getProductOptions($orderItem, $product, $parents);
+      $orderItemExtension->setOptions($options);
+    }
+
+
+    $orderItem->setExtensionAttributes($orderItemExtension);
+  }
+
+  private function getProductOptions($orderItem, $product, $parents)
+  {
+    $storeId = $orderItem->getStoreId();
+    $productOptions = [];
+
+    if (!$storeId) {
+      return null;
+    }
+
+    try {
+      $productType = $product->getTypeId();
+      if ($productType == \Magento\Catalog\Model\Product\Type::TYPE_SIMPLE || $productType == \Magento\Catalog\Model\Product\Type::TYPE_VIRTUAL) {
+        $parentIds = $this->configurableProduct->getParentIdsByChild($product->getId());
+
+        if (!empty($parentIds)) {
+          $parentId = array_shift($parentIds);
+          $parent = $this->fintParentById($parents, $parentId);
+          $data = $parent->getTypeInstance()->getConfigurableOptions($parent);
+
+          foreach ($data as $attributes) {
+            foreach ($attributes as $prod) {
+              if ($product->getSku() == $prod['sku']) {
+                $productOptions[] = clone $this->orderItemOptions->load($prod['attribute_code'], $prod['option_title']);
+              }
+            }
+          }
+        }
+      }
+    } catch (\Magento\Framework\Exception\NoSuchEntityException $ex) {
+    }
+
+    return empty($productOptions) ? null : $productOptions;
+  }
+
+
+  /**
+   * Add cart options to extension attributes.
+   * 
+   * @return string
+   */
+  private function getProductImage($product)
+  {
+    try {
+      $imageUrl = $product->getImage();
+
+      if (!$imageUrl) {
+        return null;
+      }
+
+      return $this->storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA)
+        . 'catalog/product' . $imageUrl;
+    } catch (\Magento\Framework\Exception\NoSuchEntityException $ex) {
+    }
+
+    return null;
+  }
+
+  private function fintParentById($parentIds, $id)
+  {
+    foreach ($parentIds as $parent) {
+      if ($id == $parent->getId()) {
+        return $parent;
+      }
+    }
+
+    return null;
   }
 }
