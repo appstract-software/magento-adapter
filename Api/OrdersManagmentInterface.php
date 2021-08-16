eac30d15ccccddafeed358d547d9c06f31c8c289
BartoszSzafarek <61381599+BartoszSzafarek@users.noreply.github.com>
2021-08-16 13:55:15 +0200

    pagination (#77)
    Co-authored-by: Bartek Szafarek <bartosz.szafarek@appstract.software>

--- Api/OrdersManagmentInterface.php
+++ Api/OrdersManagmentInterface.php
@@ -8,7 +8,8 @@ interface OrdersManagmentInterface
     * Get orders for customer
     *
     * @param int $customerId
+    * @param \Magento\Framework\Api\Search\SearchCriteriaInterface $searchCriteria
     * @return \Magento\Sales\Api\Data\OrderInterface[]
     */
-   public function getListForCustomer($customerId);
+   public function getListForCustomer($customerId, $searchCriteria);
 }
