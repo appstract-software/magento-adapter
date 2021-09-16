26ae4fc94eb127b9ec358b5f9c80d76fe2153097
Bartosz Wróbel <bartoszwrobel14@gmail.com>
2021-09-16 15:51:21 +0200

    add gql order shipping method code (#104)

---
+++ CustomGQL/Model/Resolver/ShippingMethodCodeResolver.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Appstractsoftware\MagentoAdapter\CustomGQL\Model\Resolver;
+
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Sales\Model\Order;
+use Magento\Framework\Exception\LocalizedException;
+
+class ShippingMethodCodeResolver implements ResolverInterface
+{
+  /**
+   * @inheritDoc
+   */
+  public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
+  {
+    if (!isset($value['model']) && !($value['model'] instanceof Order)) {
+      throw new LocalizedException(__('"model" value should be specified'));
+    }
+    /** @var Order $order */
+    $order = $value['model'];
+
+    return $order->getShippingMethod();
+  }
+}
