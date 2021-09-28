8bc7725d8d4b792c1c1db3f2f08d4ae9f68d4b19
Bartosz Wróbel <bartoszwrobel14@gmail.com>
2021-09-28 13:59:01 +0200

    elasticsearch improvements

--- Helper/Data.php
+++ Helper/Data.php
@@ -12,6 +12,9 @@ class Data extends AbstractHelper
   const AUTO_GENERATE_INVOICE = 'auto_generate_invoice';
   const SOURCES_TO_SKIP = 'sources_to_skip';
   const CLIENT_ADMIN_GROUP_ID = 'client_admin_group_id';
+  const ELASTICSEARCH_FUZZINESS = 'elasticsearch_fuzziness';
+  const ELASTICSEARCH_SKIP_URL_KEY = 'elasticsearch_skip_url_key';
+  const ELASTICSEARCH_SKU_BOOST = 'elasticsearch_sku_boost';
 
   public function getConfigValue($field, $storeId = null)
   {
@@ -41,5 +44,32 @@ class Data extends AbstractHelper
   public function getClientAdminGroupId($storeId = null)
   {
     return $this->getConfiguration(self::CLIENT_ADMIN_GROUP_ID, $storeId);
+  }
+
+  public function getElasticsearchFuzziness($storeId = null)
+  {
+    $fuzzines =  $this->getConfiguration(self::ELASTICSEARCH_FUZZINESS, $storeId);
+
+    if ($fuzzines == '') {
+      return 'AUTO';
+    }
+
+    return $fuzzines;
+  }
+
+  public function getElasticsearchSkipUrlKey($storeId = null)
+  {
+    return $this->getConfiguration(self::ELASTICSEARCH_SKIP_URL_KEY, $storeId) == '1' ? true : false;
+  }
+
+  public function getElasticsearchSkuBoost($storeId = null)
+  {
+    $boost = $this->getConfiguration(self::ELASTICSEARCH_SKU_BOOST, $storeId);
+
+    if ($boost == '') {
+      return false;
+    }
+
+    return (int)$boost;
   }
 }
