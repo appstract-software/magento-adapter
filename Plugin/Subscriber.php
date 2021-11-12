fef88d1d6ec26b84636551107973847a62356114
Jan Klamka <jan.klamka@appstract.software>
2021-11-12 10:49:42 +0100

    asd

--- Plugin/Subscriber.php
+++ Plugin/Subscriber.php
@@ -8,10 +8,10 @@ use Magento\Newsletter\Model\Subscriber as ParentSubscriber;
 
 class Subscriber extends ParentSubscriber
 {
-  const XML_PATH_CONFIRM_EMAIL_TEMPLATE = 'newsletter/subscription/confirm_email_template';
-  const XML_PATH_CONFIRM_EMAIL_IDENTITY = 'newsletter/subscription/confirm_email_identity'; 
-  const XML_PATH_SUCCESS_EMAIL_TEMPLATE = 'newsletter/subscription/success_email_template';
-  const XML_PATH_SUCCESS_EMAIL_IDENTITY = 'newsletter/subscription/success_email_identity';
+  // const XML_PATH_CONFIRM_EMAIL_TEMPLATE = 'newsletter/subscription/confirm_email_template';
+  // const XML_PATH_CONFIRM_EMAIL_IDENTITY = 'newsletter/subscription/confirm_email_identity'; 
+  // const XML_PATH_SUCCESS_EMAIL_TEMPLATE = 'newsletter/subscription/success_email_template';
+  // const XML_PATH_SUCCESS_EMAIL_IDENTITY = 'newsletter/subscription/success_email_identity';
   // const XML_PATH_UNSUBSCRIBE_EMAIL_TEMPLATE = 'newsletter/subscription/un_email_template';
   // const XML_PATH_UNSUBSCRIBE_EMAIL_IDENTITY = 'newsletter/subscription/un_email_identity';
   // const XML_PATH_CONFIRMATION_FLAG = 'newsletter/subscription/confirm';
@@ -22,14 +22,14 @@ class Subscriber extends ParentSubscriber
     callable $proceed
   ) {
     var_dump('JESTEM TU ELO aroundSendConfirmationRequestEmail');
-    var_dump($this->getCode());
-    var_dump($this->getEmail());
+    // var_dump($this->getCode());
+    // var_dump($this->getEmail());
     var_dump($subject->getEmail());
     var_dump($subject->getCode());
     var_dump($subject->getConfirmationLink());
 
     $vars = [
-      'store' => $this->_storeManager->getStore($this->getStoreId()),
+      'store' => $subject->_storeManager->getStore($subject->getStoreId()),
       'subscriber_data' => [
         'confirmation_link' => $subject->getConfirmationLink(),
         'confirmation_code' => $subject->getCode(),
@@ -38,10 +38,10 @@ class Subscriber extends ParentSubscriber
       ],
     ];
     // var_dump($vars);
-    var_dump(self::XML_PATH_CONFIRM_EMAIL_TEMPLATE);
-    var_dump(self::XML_PATH_CONFIRM_EMAIL_IDENTITY);
+    // var_dump(self::XML_PATH_CONFIRM_EMAIL_TEMPLATE);
+    // var_dump(self::XML_PATH_CONFIRM_EMAIL_IDENTITY);
 
-    // $this->sendEmail(self::XML_PATH_CONFIRM_EMAIL_TEMPLATE, self::XML_PATH_CONFIRM_EMAIL_IDENTITY, $vars);
+    $subject->sendEmail(self::XML_PATH_CONFIRM_EMAIL_TEMPLATE, self::XML_PATH_CONFIRM_EMAIL_IDENTITY, $vars);
     return $this;
   }
 
@@ -62,35 +62,35 @@ class Subscriber extends ParentSubscriber
    * @param array $templateVars
    * @return void
    */
-  private function sendEmail(string $emailTemplatePath, string $emailIdentityPath, array $templateVars = []): void
-  {
-    if ($this->getImportMode()) {
-      return;
-    }
-    $template = $this->_scopeConfig->getValue($emailTemplatePath, ScopeInterface::SCOPE_STORE, $this->getStoreId());
-    $identity = $this->_scopeConfig->getValue($emailIdentityPath, ScopeInterface::SCOPE_STORE, $this->getStoreId());
-    if (!$template || !$identity) {
-      return;
-    }
-    $templateVars += ['subscriber' => $this];
-    $this->inlineTranslation->suspend();
-    $this->_transportBuilder->setTemplateIdentifier(
-      $template
-    )->setTemplateOptions(
-      [
-        'area' => Area::AREA_FRONTEND,
-        'store' => $this->getStoreId(),
-      ]
-    )->setTemplateVars(
-      $templateVars
-    )->setFrom(
-      $identity
-    )->addTo(
-      $this->getEmail(),
-      $this->getName()
-    );
-    $transport = $this->_transportBuilder->getTransport();
-    $transport->sendMessage();
-    $this->inlineTranslation->resume();
-  }
+  // private function sendEmail(string $emailTemplatePath, string $emailIdentityPath, array $templateVars = []): void
+  // {
+  //   if ($this->getImportMode()) {
+  //     return;
+  //   }
+  //   $template = $this->_scopeConfig->getValue($emailTemplatePath, ScopeInterface::SCOPE_STORE, $this->getStoreId());
+  //   $identity = $this->_scopeConfig->getValue($emailIdentityPath, ScopeInterface::SCOPE_STORE, $this->getStoreId());
+  //   if (!$template || !$identity) {
+  //     return;
+  //   }
+  //   $templateVars += ['subscriber' => $this];
+  //   $this->inlineTranslation->suspend();
+  //   $this->_transportBuilder->setTemplateIdentifier(
+  //     $template
+  //   )->setTemplateOptions(
+  //     [
+  //       'area' => Area::AREA_FRONTEND,
+  //       'store' => $this->getStoreId(),
+  //     ]
+  //   )->setTemplateVars(
+  //     $templateVars
+  //   )->setFrom(
+  //     $identity
+  //   )->addTo(
+  //     $this->getEmail(),
+  //     $this->getName()
+  //   );
+  //   $transport = $this->_transportBuilder->getTransport();
+  //   $transport->sendMessage();
+  //   $this->inlineTranslation->resume();
+  // }
 }
