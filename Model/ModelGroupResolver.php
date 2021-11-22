d0d009718898c3078e45f5124e3c50894dde1a59
Bartosz Wróbel <bartoszwrobel14@gmail.com>
2021-11-22 10:43:55 +0100

    fix for gql products query price when catalog price rule is set for customer group

---
+++ Model/ModelGroupResolver.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Appstractsoftware\MagentoAdapter\Model;
+
+use Appstractsoftware\MagentoAdapter\Model\ResourceModelGroupResolver as ResolverResource;
+
+/**
+ * Lightweight service for getting current customer group based on customer id
+ */
+class ModelGroupResolver
+{
+  /**
+   * @var ResolverResource
+   */
+  private $resolverResource;
+
+  /**
+   * @param ResolverResource $resolverResource
+   */
+  public function __construct(ResolverResource $resolverResource)
+  {
+    $this->resolverResource = $resolverResource;
+  }
+
+  /**
+   * Return customer group id
+   *
+   * @param int $customerId
+   * @return int|null
+   */
+  public function resolve(int $customerId): ?int
+  {
+    return $this->resolverResource->resolve($customerId);
+  }
+}
