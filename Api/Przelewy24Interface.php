58f247fc8606ce341242e466255d6adb46bd0ff1
Bartosz Wróbel <bartoszwrobel14@gmail.com>
2021-12-15 18:35:09 +0100

    Revert "p24 predefined method (#119)"
    This reverts commit 717b1281d0c4db01572ced84b2be6f147b5761a4.

--- Api/Przelewy24Interface.php
+++ Api/Przelewy24Interface.php
@@ -12,8 +12,7 @@ interface Przelewy24Interface
      *
      * @param string $orderId
      * @param string $urlReturn
-     * @param int $predefinedMethod
      * @return Appstractsoftware\MagentoAdapter\Api\Data\Przelewy24RegisterTransactionResponseInterface
      */
-    public function registerTransaction($orderId, $urlReturn, $predefinedMethod);
+    public function registerTransaction($orderId, $urlReturn);
 }
