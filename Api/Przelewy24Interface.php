504f5262a2a734c4c53255e9f4f6dc76b1a86dd4
Bartosz Wróbel <bartoszwrobel14@gmail.com>
2021-12-15 14:53:16 +0100

    p24 predefined method

--- Api/Przelewy24Interface.php
+++ Api/Przelewy24Interface.php
@@ -12,7 +12,8 @@ interface Przelewy24Interface
      *
      * @param string $orderId
      * @param string $urlReturn
+     * @param int $predefinedMethod
      * @return Appstractsoftware\MagentoAdapter\Api\Data\Przelewy24RegisterTransactionResponseInterface
      */
-    public function registerTransaction($orderId, $urlReturn);
+    public function registerTransaction($orderId, $urlReturn, $predefinedMethod);
 }
